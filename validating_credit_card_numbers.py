2b5315b8d1afd868f248135ee2a702fa82598918
Borja Gutierrez Miralles <63304964+BGMiralles@users.noreply.github.com>
2022-08-09 18:47:06 -0500

    Update validating_credit_card_numbers.py

--- validating_credit_card_numbers.py
+++ validating_credit_card_numbers.py
@@ -1,3 +1,18 @@
+# A valid credit card from ABCD Bank has the following characteristics:
+#     ► It must start with a , or .
+#     ► It must contain exactly digits.
+#     ► It must only consist of digits (-).
+#     ► It may have digits in groups of , separated by one hyphen "-".
+#     ► It must NOT use any other separator like ' ' , '_', etc.
+#     ► It must NOT have or more consecutive repeated digits. 
+
+# Input Format:
+#     The first line of input contains an integer `N`.
+#     The next `N` lines contain credit card numbers. 
+
+# Output Format:
+#     Print 'Valid' if the credit card number is valid. Otherwise, print 'Invalid'. Do not print the quotes.
+    
 import re
 
 
