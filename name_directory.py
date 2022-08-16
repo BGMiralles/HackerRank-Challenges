4c00fd9551522d6809bd0c523c4720ae21bd60d0
Borja Gutierrez Miralles <63304964+BGMiralles@users.noreply.github.com>
2022-08-16 17:51:09 -0500

    Update name_directory.py

--- name_directory.py
+++ name_directory.py
@@ -7,7 +7,7 @@
 
 def person_lister(f):
     def inner(people):
-        return map(f, sorted(people, key=lambda x: x[2]))
+        return map(f, sorted(people, key=lambda x: int(x[2])))
     return inner
 
 
